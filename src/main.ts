b516216fa465ae2ec291aa3add01281e294dd1ff
jjam02 <35983461+jjam02@users.noreply.github.com>
2023-12-08 09:15:50 -0800

    remove console log for color

--- src/main.ts
+++ src/main.ts
@@ -42,8 +42,6 @@ function randomPolyLineColor(): string {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
-  console.log("rgb(" + r + " ," + g + "," + b + ")");
-
   return "rgb(" + r + " ," + g + "," + b + ")";
 }
 
